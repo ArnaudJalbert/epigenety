f9c58eb0a4917c35649dee3c1448a284cdb5a341
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2023-03-14 17:59:16 -0400

    added info.py

---
+++ info.py
@@ -0,0 +1,10 @@
+import os
+# instagram
+INSTA_USERNAME = os.getenv('INSTA_USERNAME')
+INSTA_PASSWORD = os.getenv('INSTA_PASSWORD')
+# open ai
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# API endpoint for DALL-E
+DALL_E_URL = "https://api.openai.com/v1/images/generations"
+
