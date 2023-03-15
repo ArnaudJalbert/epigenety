e9f523d03d2242bd1e3c7107c0169650b858f83c
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2023-03-14 23:31:25 -0400

    debug

--- info.py
+++ info.py
@@ -5,6 +5,8 @@ INSTA_PASSWORD = os.getenv('INSTA_PASSWORD')
 # open ai
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+print(INSTA_USERNAME)
+
 # API endpoint for DALL-E
 DALL_E_URL = "https://api.openai.com/v1/images/generations"
 
